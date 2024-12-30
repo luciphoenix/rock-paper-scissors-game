32aace29419919a6c8c69776753743b97c8e6841
Luciphoenix <luciphoenix.soft@gmail.com>
2024-12-30 13:49:09 +0000

    Get the game to partial completion

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
+
+let humanChoice;
+let computerChoice;
+
 // Get human choice
-
 function getHumanChoice() {
-  let humanChoice = prompt("Choose either rock, paper, or scissors: ");
-    setChoiceToNumber(humanChoice)
+  humanChoice = prompt("Choose either rock, paper, or scissors: ");
+    humanChoice=setChoiceToNumber(humanChoice)
+    console.log(humanChoice);
 }
-
 
 function setChoiceToNumber(choice) {
      if (choice == "rock") {
-    choice = 1;
+   return 1;
   } else if (choice == "paper") {
-    choice = 2;
+    return 2;
   } else {
-    choice = 3;
+    return 3;
   }
-  console.log(choice);
-  
+   
 }
+
+// Get computer choice
+
+function generateComputerChoice() {
+  let random = Math.floor(Math.random() * 10);
+  if (random == 1 || random == 4 || random == 7) {
+    computerChoice = 1;
+  } else if (random == 2 || random == 5 || random == 8) {
+    computerChoice = 2;
+  } else {
+    computerChoice = 3;
+  }
+  console.log(computerChoice)
+}
+
+function checkChoices(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("draw");
+  } else if (
+    (humanChoice == 1 && computerChoice == 3) ||
+    (humanChoice == 3 && computerChoice == 2) ||
+    (humanChoice == 2 && computerChoice == 1)
+  ) {
+    console.log("Player wins");
+  } else {
+    console.log("Computer wins");
+  }
+}
+
+function playGame(){
+    getHumanChoice()
+  
+    generateComputerChoice()
+    checkChoices(humanChoice, computerChoice)
+}
+playGame()
